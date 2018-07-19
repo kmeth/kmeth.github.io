9b0d0ee57ac037f0cb5fe81f39fe0331519fda6d
kmeth <varga.matija@gmail.com>
2018-07-19 19:34:37 +0200

    animate css etc

--- script.js
+++ script.js
@@ -48,3 +48,24 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+
+  // Check if element is scrolled into view
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+  // If element is scrolled into view, fade it in
+  $(window).scroll(function() {
+    $('.animated').each(function() {
+      if (isScrolledIntoView(this) === true) {
+        $(this).addClass('fadeInUp');
+        $(this).css("visibility","visible");
+      }
+    });
+  });
